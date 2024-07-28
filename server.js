6467d0674dbd36888f0dad7b96dfad4a5bfcf122
PankajYadavNitn <pankajyadav75123@gamil.com>
2024-07-28 10:53:40 +0530

    Final Code
    Ecommerce Mern code

---
+++ server.js
@@ -0,0 +1,42 @@
+import express  from "express";
+import colors from "colors";
+import dotenv, { configDotenv } from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import authRoutes from './routes/authRoute.js'
+import cors from "cors"
+import categoryRoutes from './routes/categoryRoutes.js'
+import productRoutes from  './routes/productRoutes.js'
+import path from "path"
+//configure env
+dotenv.config();
+
+//database config
+connectDB();
+
+//rest object
+const app = express();
+
+//middlewares
+app.use(cors())
+app.use(express.json())
+app.use(morgan('dev'))
+app.use(express.static(path.join(__dirname,'./cleint/build')))
+
+//routes
+app.use("/api/v1/auth",authRoutes);
+app.use("/api/v1/category",categoryRoutes);
+app.use("/api/v1/product",productRoutes)
+
+//rest api
+app.use('*',function(req,res){
+    res.sendFile(path.join(__dirname,'./cleint/build/index.html'))
+})
+//PORT
+const PORT = process.env.PORT ||8081;
+
+//run listen
+app.listen(PORT,() =>{
+    console.log(`server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white);
+})
+
